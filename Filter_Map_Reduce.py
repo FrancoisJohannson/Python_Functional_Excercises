898acd37aa21f224f5a3bac7df2f01696c927888
FrancoisJohannson <erhard.janak@t-online.de>
2020-02-16 14:21:02 +0100

    filter map reduce

--- Filter_Map_Reduce.py
+++ Filter_Map_Reduce.py
@@ -1,4 +1,16 @@
-list_X = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+from functools import reduce
+from typing import List
 
+list_X: List[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# get all uneven numbers
 filter_object = filter(lambda it: it % 2, list_X)
 print(list(filter_object))
+
+# square every element
+map_object = map(lambda it: it * it, list_X)
+print(list(map_object))
+
+# the sum of all elements
+reduce_result = reduce(lambda accumulator, it: accumulator + it, list_X)
+print(reduce_result)
